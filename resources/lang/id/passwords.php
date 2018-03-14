9ad93e270396d5e15140c2b24cecee6dad6bc77d
Bintang Muhammad <bintangmhd@gmail.com>
2018-03-14 14:47:27 +0700

    adding language

---
+++ resources/lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'reset' => 'Password anda telah di-reset!',
+    'sent' => 'Kami sudah mengirim tautan untuk mereset password anda!',
+    'token' => 'Token reset password tidak valid',
+    'user' => "Kami tidak bisa menemukan pengguna dengan alama email tersebut.",
+
+];
