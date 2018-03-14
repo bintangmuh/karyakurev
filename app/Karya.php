540c74590d1653867a43b3fb78f2b9d918ee3cf0
Bintang Muhammad <bintangmhd@gmail.com>
2018-03-14 14:48:08 +0700

    adding relation

--- app/Karya.php
+++ app/Karya.php
@@ -8,5 +8,15 @@ class Karya extends Model
 {
     protected $table = "karya";
 
-    
+	public function gallery() {
+		return $this->hasMany('App\Gallery', 'karya_id');
+	}
+
+	public function video() {
+		return $this->hasMany('App\Video', 'karya_id');
+	}
+
+	public function user() {
+		return $this->belongsTo('App\User', 'user_id');
+	}
 }
