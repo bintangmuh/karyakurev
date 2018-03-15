5777327f3b9db2acf32d2c80d0d045856fac53f3
Bintang Muhammad <bintangmhd@gmail.com>
2018-03-15 20:41:35 +0700

    adding validation to form BS3 to BS4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,28 +14,28 @@
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-8">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="form-control {{ $errors->has('email') ? ' is-invalid ' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group row{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-8">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control {{ $errors->has('password') ? ' is-invalid ' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
@@ -59,7 +59,7 @@
                                 </button>
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
+                                    Lupa password anda?
                                 </a>
                             </div>
                         </div>
--- resources/views/indexpage.blade.php
+++ resources/views/indexpage.blade.php
@@ -13,31 +13,31 @@
                         <div class="card-body">
                             <b>Login</b>
                             <hr>
-                            <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                             <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                                 {{ csrf_field() }}
 
-                                <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <div class="form-group row">
                                     <label for="email" class="col-md-4 control-label">Email</label>
 
                                     <div class="col-md-8">
-                                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                        <input id="email" type="email" class="form-control {{ $errors->has('email') ? ' is-invalid ' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                                         @if ($errors->has('email'))
-                                            <span class="help-block">
+                                            <span class="invalid-feedback">
                                                 <strong>{{ $errors->first('email') }}</strong>
                                             </span>
                                         @endif
                                     </div>
                                 </div>
 
-                                <div class="form-group row{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <div class="form-group row">
                                     <label for="password" class="col-md-4 control-label">Password</label>
 
                                     <div class="col-md-8">
-                                        <input id="password" type="password" class="form-control" name="password" required>
+                                        <input id="password" type="password" class="form-control {{ $errors->has('password') ? ' is-invalid ' : '' }}" name="password" required>
 
                                         @if ($errors->has('password'))
-                                            <span class="help-block">
+                                            <span class="invalid-feedback">
                                                 <strong>{{ $errors->first('password') }}</strong>
                                             </span>
                                         @endif
@@ -48,7 +48,7 @@
                                     <div class="col-md-6 offset-md-4">
                                         <div class="checkbox">
                                             <label>
-                                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Ingat saya
+                                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Ingat akun saya
                                             </label>
                                         </div>
                                     </div>
@@ -59,6 +59,10 @@
                                         <button type="submit" class="btn btn-primary">
                                             Login
                                         </button>
+
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            Lupa password anda?
+                                        </a>
                                     </div>
                                 </div>
                             </form>
