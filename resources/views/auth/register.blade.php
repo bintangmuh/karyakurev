73e7825e39577c848acf5da86406df43d0545620
Bintang Muhammad <bintangmhd@gmail.com>
2018-03-18 22:22:05 +0700

    adding register validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,56 +16,56 @@
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group row{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <div class="form-group row">
                             <label for="name" class="col-md-4 control-label">Nama Lengkap</label>
 
                             <div class="col-md-8">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="name" type="text" class="form-control {{ $errors->has('name') ? ' is-invalid ' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group row{{ $errors->has('username') ? ' has-error' : '' }}">
+                        <div class="form-group row">
                             <label for="username" class="col-md-4 control-label">Username</label>
 
                             <div class="col-md-8">
-                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
+                                <input id="username" type="text" class="form-control {{ $errors->has('username') ? ' is-invalid ' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
 
                                 @if ($errors->has('username'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback">
                                         <strong>{{ $errors->first('username') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 control-label">Alamat Email</label>
 
                             <div class="col-md-8">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control {{ $errors->has('email') ? ' is-invalid ' : '' }}" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group row{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-8">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control {{ $errors->has('password') ? ' is-invalid ' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
@@ -76,7 +76,7 @@
                             <label for="password-confirm" class="col-md-4 control-label">Ulang Password</label>
 
                             <div class="col-md-8">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <input id="password-confirm" type="password" class="form-control {{ $errors->has('password_confirmation') ? ' is-invalid ' : '' }}" name="password_confirmation" required>
                             </div>
                         </div>
 
