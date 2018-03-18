3b6afc0749246adad53e230821e2ea7599c33246
Bintang Muhammad <bintangmhd@gmail.com>
2018-03-18 22:39:01 +0700

    adding admin master blade
    i just think using CoreUI for admin template

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/fontawesome-all.min.css') }}">
+    <link rel="shortcut icon" href="{{ asset('/img/favicon.png') }}" type="image/x-icon">
+    <meta name="theme-color" content="#0093E9">
+    <title>@yield('title') Administrator Karyaku</title>
+</head>
+<body>
+    <!-- Admin Top Bar  -->
+    <!-- Admin Side Bar -->
+    @yield('content')    
+    
+        
+        
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('/js/vue.js') }}"></script>
+    <script src="{{ asset('/js/axios.min.js') }}"></script>
+    @stack('js')
+</body>
+</html> 
