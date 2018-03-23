325053cc9ef59f80d21ca373adc4aca05b186816
Bintang Muhammad <bintangmhd@gmail.com>
2018-03-23 17:10:18 +0700

    koreksi layout
    penampilan waktu masih murni database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,10 @@
 @extends('master')
-
 @section('title', 'Beranda - ')
-
 @section('content')
 @include('components.navbar')
 <div class="container">
     <div class="row">
-        <div class="col-sm-3">
+        <div class="col-md-3">
             <div class="card d-none d-md-block">
                 <div class="card-body">
                     <img src="{{ asset('img/noprofilimage.png') }}"  class="rounded-circle" alt=""> <br>
@@ -15,34 +13,38 @@
                     {{ Auth::user()->prodi }}
                 </div>
             </div>
-            <a href="{{ route('karya.buatview') }}" class="btn btn-primary btn-block">Tambah Karya Baru</a>
+            <a href="{{ route('karya.buatview') }}" class="btn btn-primary btn-block mt-3 mb-3"><i class="fa fa-file"></i> Tambah Karya Baru</a>
         </div>
-        <div class="col-sm-9">
+        <div class="col-md-9">
             <div class="card">
-                {{-- <div class="card-header bg-primary" style="color: #fff"><b>Karya yang baru diunggah ... </b></div> --}}
-
+                {{-- <div class="card-header bg-prixmary" style="color: #fff"><b>Karya yang baru diunggah ... </b></div> --}}
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
                     @endif
-
-
                     @foreach ($karya as $karyatunggal)
-                        <div class="media media-newsfeed" style="padding-bottom: 20px;">
-                          <img class="mr-3 img-thumbnail" src="{{ asset('img/noimage.png') }}" alt="Generic placeholder image">
-                          <div class="media-body">
-                            <h5 class="mt-0"><a href="{{ route('karya.tampil', ['karya' => $karyatunggal]) }}"><b>{{ $karyatunggal->nama }}</b></a></h5>
-                            {{ $karyatunggal->deskripsi }}
-                          </div>
+                    <div class="media media-newsfeed" style="padding-bottom: 20px;">
+                        <a href="{{ route('karya.tampil', ['karya' => $karyatunggal]) }}"><img class="mr-3 img-thumbnail" src="{{ asset('img/noimage.png') }}" alt="Generic placeholder image"></a>
+                        
+                        <div class="media-body">
+                            <span class="title"><a href="{{ route('karya.tampil', ['karya' => $karyatunggal]) }}"><b>{{ $karyatunggal->nama }}</b></a></span>
+                            <span class="date">{{ $karyatunggal->created_at }}</span>
+                            <span class="author">
+                                <img src="{{ asset('img/noprofilimage.png') }}" height="16pt" class="rounded-circle" alt=""> {{ $karyatunggal->user->name }}
+                            </span>
+                            <div class="desc d-none d-md-block">
+                                {{ $karyatunggal->deskripsi }}
+                            </div>
                         </div>
+                    </div>
                     @endforeach
                     
                     
                 </div>
             </div>
-            <a href="#" class="btn btn-default">Lihat Lebih Banyak</a>
+            <a href="#" class="btn btn-light mt-3 mb-3 btn-block  mx-auto"><i class="fa fa-arrow-down"></i> Lihat Lebih Banyak</a>
         </div>
     </div>
 </div>
