43a039da97b2cccc5cb60ee498f43ea520118e6c
Bintang Muhammad <bintangmhd@gmail.com>
2018-03-31 21:31:12 +0700

    mbenakne admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-
+<div class="container">	
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam obcaecati aliquid, rem alias, aspernatur nulla placeat sequi quas, iusto corporis quia accusamus cumque nesciunt quo nisi aperiam laborum autem. Eius.
+</div>
 @endsection
