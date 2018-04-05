b0c420c150c6514cd17cc8ce99850bf52be5713e
Bintang Muhammad <bintangmhd@gmail.com>
2018-04-05 21:14:32 +0700

    adding report table

---
+++ database/migrations/2018_04_05_100121_create_report_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('pelapor');
+            $table->string('alasan');
+            $table->integer('karya_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('karya_id')->references('id')->on('karya')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
