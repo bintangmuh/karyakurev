7f983535074466723c575bff03dd88dcc1174f36
Bintang Muhammad <bintangmhd@gmail.com>
2018-04-08 21:52:21 +0700

    adding icons

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-gradient">
   <div class="container">
-    <a class="navbar-brand" href="#"><img src="{{ asset('/img/karyaku-white-full-200px.png') }}" alt="karyaku logo" height="30" class="img-responsive"></a>
+    <a class="navbar-brand" href="{{ url('/home')}}"><img src="{{ asset('/img/karyaku-white-full-200px.png') }}" alt="karyaku logo" height="30" class="img-responsive"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -9,29 +9,29 @@
       @if (Auth::check())
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link {{ Route::currentRouteName() == "home" ? 'active' : '' }}" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link {{ Route::currentRouteName() == "home" ? 'active' : '' }}" href="{{ route('home') }}"><i class="fa fa-home"></i> Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{ Route::currentRouteName() == "profile" ? 'active' : '' }}" href="{{ route('profile') }}">Profile</a>
+            <a class="nav-link {{ Route::currentRouteName() == "profile" ? 'active' : '' }}" href="{{ route('profile') }}"><i class="fa fa-user"></i> Profile</a>
           </li>
         </ul>
       @endif
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-          <a class="nav-link {{ Route::currentRouteName() == "explore" ? 'active' : '' }}" href="{{ route('explore') }}">Jelajah</a>
+          <a class="nav-link {{ Route::currentRouteName() == "explore" ? 'active' : '' }}" href="{{ route('explore') }}"><i class="fa fa-compass"></i>  Jelajah</a>
         </li>
         @if (Auth::check())
           <li class="nav-item dropdown" style="color: black">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fa fa-gear"></i> Setting
+                <i class="fa fa-cogs"></i> Setting
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ url('/profile/edit') }}">Edit Profile</a>
+                <a class="dropdown-item" href="{{ url('/profile/edit') }}"><i class="fa fa-wrench"></i> Edit Profile</a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="{{ route('logout') }}"
                   onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();">
-                  Logout
+                  <i class="fa fa-sign-out-alt"></i>  Logout
                 </a>
 
               </div>
@@ -42,10 +42,10 @@
           </li>
         @else
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('login') }}">Login</a>
+            <a class="nav-link" href="{{ route('login') }}"><i class="fa fa-sign-in-alt"></i> Login</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('register') }}">Register</a>
+            <a class="nav-link" href="{{ route('register') }}"><i class="fa fa-user-plus"></i> Register</a>
           </li>
         @endif
       </ul>
