0c13dcc216afb1462cef8fa17775b875578ee61f
Bintang Muhammad <bintangmhd@gmail.com>
2018-04-09 10:51:49 +0700

    adding relation report to karya

--- app/Report.php
+++ app/Report.php
@@ -8,4 +8,9 @@ class Report extends Model
 {
     protected $table = 'reports';
     protected $fillable  = ['alasan', 'pelapor', 'karya_id'];
+
+    public function karya()
+    {
+    	return $this->belongsTo('App\Karya', 'karya_id', 'id');
+    }
 }
