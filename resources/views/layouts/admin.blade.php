6be49894cedd167b80234a382920b16cf8638b67
Bintang Muhammad <bintangmhd@gmail.com>
2018-04-22 08:57:38 +0700

    cdn change

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -32,9 +32,9 @@
         </div>        
         
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="{{ asset('/js/bootstrap.min.js') }}"></script>
-        <script src="{{ asset('/js/vue.min.js') }}"></script>
-        <script src="{{ asset('/js/axios.min.js') }}"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.min.js"></script>
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
         <script src="{{ asset('/js/admin/app.js') }}"></script>
         @stack('js')
     </body>
